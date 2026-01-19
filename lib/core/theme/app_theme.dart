82d1d46c348b9ee0f53110305187eb44649ce4ea
Abdul Samad <abdulsamad639263@gmail.com>
2026-01-19 17:04:26 +0530

    Add Material 3 app theme with dark mode support

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.light,
+    ),
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.dark,
+    ),
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+    ),
+  );
+}
